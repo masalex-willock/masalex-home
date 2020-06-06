1fdc71c43a8cf6de4322cd3c5d648bbed8ac02cc
masalex tom <webdesignerns@gmail.com>
2020-06-06 14:41:20 +0300

    june 6 2020, upload to github

--- javascript/main.js
+++ javascript/main.js
@@ -6,6 +6,7 @@ function clicked(){
 	let weight = Number(prompt('Enter your weight in kilos', 40)), BMI;
 	let height = Number(prompt('enter your in metres eg 1.65', 1.65));
 	BMI = weight / (height * height);
-	
-	return alert(`your BMI is ${Math.floor(BMI, 2)}`)
+	if(BMI < 18) return alert(`your BMI is ${Math.round(BMI)} \n need to begin eating enough because that BMI is low`);
+	else if(BMI >= 18 && BMI < 27) return alert(`your BMI is ${Math.round(BMI, 2)}, \n congratulations bacause your within normal ranges, keep-up`);
+	else return alert(`your BMI is ${Math.round(BMI, 2)} ☁ \nNo offence but your really fat\n cut down on the carbohydrate that you eat then for christ\'s sake do some exercise'`)
 }
